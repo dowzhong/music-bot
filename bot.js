a88bdc901a3c76fe7ac8662b0b2d76e9ba3e54c6
cthuluhoop123 <dowzhong@gmail.com>
2019-03-27 19:02:23 +1100

    more stuff

--- bot.js
+++ bot.js
@@ -29,19 +29,46 @@ client.on('message', async message => {
     const args = message.content.split(' ')
     const command = args.shift().toLowerCase().slice(prefix.length)
 
+    if (command === 'skip') {
+        const guildData = client.database.get(message.guild.id)
+        if (!guildData.playlist.length) {
+            message.channel.send('<:error:560328317505372170> You have no songs to skip.')
+            return
+        }
+        const songToSkip = parseInt(args[0]) || 0
+
+        const [deleted] = guildData.playlist.splice(songToSkip, 1)
+        message.channel.send(`<:success:560328302523580416> Skipped **${deleted.title} - ${deleted.channelName}**.`)
+
+        if (songToSkip === 0) { guildData.connection.dispatcher.end('skipped') }
+    }
+
+    if (command === 'stop') {
+        const guildData = client.database.get(message.guild.id)
+        if (!guildData.connection) {
+            message.channel.send('<:error:560328317505372170> I\'m not playing anything right now.')
+            return
+        }
+        guildData.connection.dispatcher.end('stopped')
+        guildData.voiceChannel.leave()
+        guildData.playlist = []
+        message.channel.send(`<:success:560328302523580416> Stopped playing music.`)
+    }
+
     if (command === 'queue') {
         const guildData = client.database.get(message.guild.id)
         if (!guildData) {
-            message.channel.send('❌ You have no songs queued')
+            message.channel.send('<:error:560328317505372170> You have no songs queued')
             return
         }
 
         const songs = guildData.playlist.map((song, i) => {
-            return `**${i + 1}.** ${song.title}\n`
+            return `${i === 0 ? `**Now Playing:** ${song.title} [${song.requestedBy.displayName}]\n` : `**${i}.** ${song.title} [${song.requestedBy.displayName}]\n`}`
         })
 
         const embed = new Discord.RichEmbed()
             .setColor(0xA787F1)
+            .setAuthor(`Song Queue`, client.user.displayAvatarURL)
             .setDescription(songs)
             .setTimestamp()
         message.channel.send({ embed })
@@ -50,89 +77,105 @@ client.on('message', async message => {
     if (command === 'play') {
 
         if (!message.member.voiceChannel) {
-            message.channel.send('❌ You must be in a voice channel first!')
+            message.channel.send('<:error:560328317505372170> You must be in a voice channel first!')
             return
         }
 
         if (!message.member.voiceChannel.joinable) {
-            message.channel.send('❌ I cannot join your voice channel. You might need to modify my permissions or join another channel.')
+            message.channel.send('<:error:560328317505372170> I cannot join your voice channel. You might need to modify my permissions or join another channel.')
             return
         }
 
         const youtubeLink = args[0].match(youtubeRegex)
+
+
+
         if (youtubeLink) {
-            //is a youtube link. Handle that link with ytdl-core
+            //is a youtube link. Handle that link with ytdl-core        
+
             try {
                 const { title, author: { name: channelName }, length_seconds: length } = await ytdl.getBasicInfo(youtubeLink[0], { filter: 'audioonly' })
-                const guildPlaylist = client.database.get(message.guild.id)
-
-                if (guildPlaylist) {
-                    guildPlaylist.playlist.push({
+                let guildData = client.database.get(message.guild.id)
+                if (guildData) {
+                    guildData.playlist.push({
                         title,
                         channelName,
                         length,
                         requestedBy: message.member,
-                        song: youtubeLink[0]
+                        song: youtubeLink[0],
+                        voiceChannel: message.member.voiceChannel
                     })
 
-                    message.channel.send(`✔ Added **${title} - ${channelName} (${parseSeconds(length)})** to playlist.`)
+                    message.channel.send(`<:success:560328302523580416> Added **${title} - ${channelName} (${parseSeconds(length)})** to playlist.`)
                     return
                 }
 
-                client.database.set(message.guild.id, {
+                guildData = {
                     playing: false,
+                    voiceChannel: message.member.voiceChannel,
                     playlist: [
                         {
                             title,
                             channelName,
                             length,
                             requestedBy: message.member,
-                            song: youtubeLink[0]
+                            song: youtubeLink[0],
                         }
                     ]
-                })
+                }
+
+                client.database.set(message.guild.id, guildData)
 
                 try {
                     const connection = await message.member.voiceChannel.join()
-
+                    guildData.connection = connection
 
                     async function play(playlistItem) {
+                        if (!playlistItem) {
+                            guildData.voiceChannel.leave()
+                            client.database.delete(message.guild.id)
+                            return
+                        }
                         const dispatcher = connection.playOpusStream(await ytdlDiscord(playlistItem.song, { passes: 3 }))
-                        message.channel.send(`✔ Started playing **${playlistItem.title} - ${playlistItem.channelName} (${parseSeconds(playlistItem.length)})**`)
+                        message.channel.send(`<:success:560328302523580416> Started playing **${playlistItem.title} - ${playlistItem.channelName} (${parseSeconds(playlistItem.length)})**`)
                         dispatcher.on('end', reason => {
-                            const { playlist } = client.database.get(message.guild.id)
-                            playlist.shift()
-                            play(playlist[0])
+                            const { playlist } = guildData
+                            if (reason !== 'skipped') { playlist.shift() }
+                            if (reason !== 'stopped') { play(playlist[0]) }
                         })
+                        dispatcher.on('error', console.error)
                     }
 
-                    play(client.database.get(message.guild.id).playlist[0])
+                    play(guildData.playlist[0])
 
                 } catch (err) {
                     console.error(err)
-                    message.channel.send('❌ Encountered an unexpected error while joining your voice channel.')
+                    message.channel.send('<:error:560328317505372170> Encountered an unexpected error while joining your voice channel.')
                 }
 
             } catch (err) {
-                message.channel.send('❌ There was an error loading that video.')
+                message.channel.send('<:error:560328317505372170> There was an error loading that video.')
             }
         }
     }
-
-    //     //input is a search term.
-    //     youtube(
-    //         'music', youtubeOptions, function (err, results) {
-    //             if (err) return console.log(err)
-    //             console.log(results)
-    //         })
-    // }
-
 })
+
+client.login(process.env.TOKEN)
+
+function searchYoutube(terms) {
+    return new Promise((resolve, reject) => {
+        youtube(terms, youtubeOptions, (err, results) => {
+            if (err) {
+                reject(err)
+                return
+            }
+            resolve(results)
+        })
+    })
+}
 
 function parseSeconds(seconds) {
     const minutes = Math.floor(seconds / 60) || '00'
     const remainingSeconds = seconds % 60 || '00'
     return `${minutes} min ${remainingSeconds}s`
 }
-
-client.login(process.env.TOKEN)
